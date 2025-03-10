568723031dba1d61cb972a465968f645231ad204
Natcha501 <40329@prot.ac.th>
2025-03-10 15:51:10 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+inp_password = input("please enter your passwoed")
+password = "python is awesone"
+
+if inp_password == password :
+    print("ACCESS GRANTED")
+
+else :
+    print("ACCESS DENIED")
+    
