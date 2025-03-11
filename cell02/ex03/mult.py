4740e5955889cc6683e4bc451b596ca575b66759
Natcha501 <40329@prot.ac.th>
2025-03-11 09:51:33 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+number_9 = int(input("enter the first number"))
+number_6 = int(input("enter the second number"))
+
+result = number_9 * number_6
+print(number_9,"x",number_6,"=",result)
+
+if result == 0 :
+ print("the result is both side negative and positive")
+
+if result > 0 :
+  print("the result is positive")
+
+if result < 0 :
+ print("the result is negative")
