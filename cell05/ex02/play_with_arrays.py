6172bc0f8ca888a2f4ee40c911d6b6997b092ea8
Natcha501 <40329@prot.ac.th>
2025-03-12 09:28:54 +0700

    Add files via upload

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,9 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+filtered_array = [value for value in original_array if value > 5]
+
+processed_array = [value + 2 for value in filtered_array]
+
+print(original_array)
+
+print(processed_array)
